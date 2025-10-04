ebcf36fb329080f9f2a073cac22e2c4dcec8bb66
dimonp4ik <dimapetrov.2006.12@gmail.com>
2025-10-04 10:42:20 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -10,12 +10,11 @@ def ping_self():
     def ping():
         while True:
             try:
-                # Твой реальный URL
                 requests.get("https://tusa-bot-1.onrender.com/")
                 print("Pinged successfully!")
             except Exception as e:
                 print(f"Ping failed: {e}")
-            time.sleep(300)  # Пинг каждые 5 минут
+            time.sleep(300)
     
     ping_thread = Thread(target=ping, daemon=True)
     ping_thread.start()
@@ -29,10 +28,6 @@ def run():
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    # Запускаем пингер
     ping_self()
-    # Запускаем Flask сервер
     t = Thread(target=run)
     t.start()
-
-
